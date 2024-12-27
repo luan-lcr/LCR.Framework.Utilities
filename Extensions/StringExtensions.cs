676a006c019ef417ced37cf8acce6be6e9c3171d
Luan Caetano Rechi <luanlcr@hotmail.com>
2024-12-27 14:14:11 -0300

    AB#17 implementado extensões simples do tipo string

---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace System;
+
+public static class StringExtensions
+{
+    public static bool IsNullOrEmpty(this string value)
+    {
+        return string.IsNullOrEmpty(value);
+    }
+
+    public static bool IsNullOrWhiteSpace(this string value)
+    {
+        return string.IsNullOrWhiteSpace(value);
+    }
+
+    public static string NullToEmpty(this string value)
+    {
+        return value ?? string.Empty;
+    }
+
+    public static string Join(this IEnumerable<string> values, string separator)
+    {
+        return string.Join(separator, values);
+    }
+}
