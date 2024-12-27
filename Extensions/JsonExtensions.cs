97eb3da8ba6cf1a2a724a1652c481f2ec98ec532
Luan Caetano Rechi <luanlcr@hotmail.com>
2024-12-27 15:35:35 -0300

    AB#11 AB#12 adicionado extensão de serialização e desserialização json

---
+++ Extensions/JsonExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace System.Text.Json;
+
+public static class JsonExtensions
+{
+    public static string ToJson<T>(this T value, JsonSerializerSettings settings)
+    {
+        return JsonConvert.SerializeObject(value, settings);
+    }
+
+    public static T JsonTo<T>(this string json, JsonSerializerSettings settings)
+    {
+        return JsonConvert.DeserializeObject<T>(json, settings);
+    }
+}
